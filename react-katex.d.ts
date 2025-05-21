73fc00e98e25a6502ff6b319c7054977e06a9a79
Anye Happiness Ade <innovacttechnologies@gmail.com>
2025-05-21 17:57:25 +0100

    `Add TypeScript declaration file for react-katex`
    This commit message is appropriate because:
    - Uses imperative mood ("Add")
    - Clearly states what was added (TypeScript declaration file)
    - Specifies which package it's for (react-katex)
    - Within recommended character limit
    - Indicates purpose (TypeScript support/type definitions)

---
+++ react-katex.d.ts
@@ -0,0 +1,16 @@
+// react-katex.d.ts
+declare module 'react-katex' {
+  import * as React from 'react';
+  export interface InlineMathProps {
+    math: string;
+    errorColor?: string;
+    renderError?: (error: Error) => React.ReactNode;
+  }
+  export interface BlockMathProps {
+    math: string;
+    errorColor?: string;
+    renderError?: (error: Error) => React.ReactNode;
+  }
+  export const InlineMath: React.FC<InlineMathProps>;
+  export const BlockMath: React.FC<BlockMathProps>;
+} 
